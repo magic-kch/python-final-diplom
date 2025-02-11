7101b50bc5c675a9e33dceee70b586bebb9f0361
magic.kch <magic.kch@gmail.com>
2025-02-11 17:29:51 +0300

    Добавления view функий и url для пользователей: Регистрация, Активация, Авторизация, Изменение

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 # from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
 
-from backend.views import PartnerUpdate
+from backend.views import PartnerUpdate, ConfirmAccount, RegisterAccount, LoginAccount, AccountDetails
 
 app_name = 'backend'
 
 urlpatterns = [
-                path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
-                # path('password_reset/', reset_password_request_token, name='password_reset'),
-                # path('password_reset/confirm/', reset_password_confirm, name='password_reset_confirm'),
+    path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
+    path('user/register', RegisterAccount.as_view(), name='user-register'),
+    path('user/register/confirm', ConfirmAccount.as_view(), name='user-register-confirm'),
+    path('user/login', LoginAccount.as_view(), name='user-login'),
+    path('user', AccountDetails.as_view(), name='user-details'),
+    # path('password_reset/', reset_password_request_token, name='password_reset'),
+    # path('password_reset/confirm/', reset_password_confirm, name='password_reset_confirm'),
     ]
--- backend/views.py
+++ backend/views.py
@@ -1,13 +1,196 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
 from django.core.validators import URLValidator
 from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import ValidationError
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from yaml import load as load_yaml, Loader
 from requests import get
 
-from backend.models import Shop, Product
-from backend.models import Category, Parameter
+
+from backend.models import Shop, Product, Category, Parameter, ProductParameter, OrderItem, Order, Contact, \
+    ConfirmEmailToken
+
+from backend.serializers import UserSerializer, CategorySerializer, ShopSerializer, ProductInfoSerializer, \
+    OrderItemSerializer
+
+
+class RegisterAccount(APIView):
+    """
+    Для регистрации покупателей
+    """
+
+    # Регистрация методом POST
+
+    def post(self, request, *args, **kwargs):
+        """
+            Process a POST request and create a new user.
+
+            Args:
+                request (Request): The Django request object.
+
+            Returns:
+                JsonResponse: The response indicating the status of the operation and any errors.
+            """
+        # проверяем обязательные аргументы
+        if {'first_name', 'last_name', 'email', 'password', 'company', 'position'}.issubset(request.data):
+
+            # проверяем пароль на сложность
+            try:
+                validate_password(request.data['password'])
+            except Exception as password_error:
+                error_array = []
+                # noinspection PyTypeChecker
+                for item in password_error:
+                    error_array.append(item)
+                return JsonResponse({'Status': False, 'Errors': {'password': error_array}})
+            else:
+                # проверяем данные для уникальности имени пользователя
+
+                user_serializer = UserSerializer(data=request.data)
+                if user_serializer.is_valid():
+                    # сохраняем пользователя
+                    user = user_serializer.save()
+                    user.set_password(request.data['password'])
+                    user.save()
+                    return JsonResponse({'Status': True})
+                else:
+                    return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+
+        return JsonResponse({'Status': False, 'Errors': 'Not enough arguments'})
+
+
+class ConfirmAccount(APIView):
+    """
+    Класс для подтверждения почтового адреса
+    """
+
+    # Регистрация методом POST
+    def post(self, request, *args, **kwargs):
+        """
+                Подтверждает почтовый адрес пользователя.
+
+                Args:
+                - request (Request): The Django request object.
+
+                Returns:
+                - JsonResponse: The response indicating the status of the operation and any errors.
+                """
+        # проверяем обязательные аргументы
+        if {'email', 'token'}.issubset(request.data):
+
+            token = ConfirmEmailToken.objects.filter(user__email=request.data['email'],
+                                                     key=request.data['token']).first()
+            if token:
+                token.user.is_active = True
+                token.user.save()
+                token.delete()
+                return JsonResponse({'Status': True})
+            else:
+                return JsonResponse({'Status': False, 'Errors': 'Неправильно указан токен или email'})
+
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
+
+
+class AccountDetails(APIView):
+    """
+    A class for managing user account details.
+
+    Methods:
+    - get: Retrieve the details of the authenticated user.
+    - post: Update the account details of the authenticated user.
+
+    Attributes:
+    - None
+    """
+
+    # получить данные
+    def get(self, request: Request, *args, **kwargs):
+        """
+               Retrieve the details of the authenticated user.
+
+               Args:
+               - request (Request): The Django request object.
+
+               Returns:
+               - Response: The response containing the details of the authenticated user.
+        """
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
+
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    # Редактирование методом POST
+    def post(self, request, *args, **kwargs):
+        """
+                Update the account details of the authenticated user.
+
+                Args:
+                - request (Request): The Django request object.
+
+                Returns:
+                - JsonResponse: The response indicating the status of the operation and any errors.
+                """
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
+        # проверяем обязательные аргументы
+
+        if 'password' in request.data:
+            errors = {}
+            # проверяем пароль на сложность
+            try:
+                validate_password(request.data['password'])
+            except Exception as password_error:
+                error_array = []
+                # noinspection PyTypeChecker
+                for item in password_error:
+                    error_array.append(item)
+                return JsonResponse({'Status': False, 'Errors': {'password': error_array}})
+            else:
+                request.user.set_password(request.data['password'])
+
+        # проверяем остальные данные
+        user_serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return JsonResponse({'Status': True})
+        else:
+            return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+
+
+class LoginAccount(APIView):
+    """
+    Класс для авторизации пользователей
+    """
+
+    # Авторизация методом POST
+    def post(self, request, *args, **kwargs):
+        """
+                Authenticate a user.
+
+                Args:
+                    request (Request): The Django request object.
+
+                Returns:
+                    JsonResponse: The response indicating the status of the operation and any errors.
+                """
+        if {'email', 'password'}.issubset(request.data):
+            user = authenticate(request, username=request.data['email'], password=request.data['password'])
+
+            if user is not None:
+                if user.is_active:
+                    token, _ = Token.objects.get_or_create(user=user)
+
+                    return JsonResponse({'Status': True, 'Token': token.key})
+
+            return JsonResponse({'Status': False, 'Errors': 'Log in unsuccessful'})
+
+        return JsonResponse({'Status': False, 'Errors': 'Not enough arguments'})
 
 
 class PartnerUpdate(APIView):
