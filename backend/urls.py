a6d3f537a197ffa801ddca7bcebc7e88dc272458
magic.kch <magic.kch@gmail.com>
2025-02-12 12:45:01 +0300

    Добавление маршрутов для Категорий и Магазинов

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 # from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
 
-from backend.views import PartnerUpdate, ConfirmAccount, RegisterAccount, LoginAccount, AccountDetails
+from backend.views import PartnerUpdate, ConfirmAccount, RegisterAccount, LoginAccount, AccountDetails, CategoryView, \
+    ShopView
 
 app_name = 'backend'
 
@@ -11,6 +12,9 @@ urlpatterns = [
     path('user/register/confirm', ConfirmAccount.as_view(), name='user-register-confirm'),
     path('user/login', LoginAccount.as_view(), name='user-login'),
     path('user', AccountDetails.as_view(), name='user-details'),
+    path('categories', CategoryView.as_view(), name='categories'),
+    path('shops', ShopView.as_view(), name='shops'),
+
     # path('password_reset/', reset_password_request_token, name='password_reset'),
     # path('password_reset/confirm/', reset_password_confirm, name='password_reset_confirm'),
     ]
