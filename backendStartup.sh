8c6249f163a21384782fc2c6043a6f9a97a90e52
magic.kch <magic.kch@gmail.com>
2025-04-28 19:25:16 +0300

    Добавлено создание миграций и создание суперпользователя

--- backendStartup.sh
+++ backendStartup.sh
@@ -3,6 +3,7 @@
 set -x
 
 echo "Начинаем миграцию базы данных..."
+python manage.py makemigrations backend
 python manage.py migrate
 echo "Миграция базы данных успешно завершена!"
 
@@ -11,7 +12,7 @@ python manage.py collectstatic --noinput
 echo "Сборка статических файлов успешно завершена!"
 
 echo "Создаем суперпользователя..."
-#TO_DO python manage.py createsuperuser --email admin@admin.ru --skip-checks
+python manage.py create_superuser
 
 echo "Начинаем запуск сервера..."
 gunicorn --bind 0.0.0.0:8000 -w 3 orders.wsgi
