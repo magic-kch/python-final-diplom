d168d847742c995e07b79881629e0cf15889115b
magic.kch <magic.kch@gmail.com>
2025-04-29 16:10:36 +0300

    Добавлены celery задачи для отправки писем, вызовы задач для асинхронной обработки

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,9 +1,10 @@
 from celery import shared_task
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
 from django.db import transaction
+from django_rest_passwordreset.models import ResetPasswordToken
 from requests import get
 from yaml import load as load_yaml, Loader
 
@@ -20,30 +21,91 @@ from backend.models import (
 
 
 @shared_task
-def reset_password_request_token(task_id):
-    print('запуск задачи celery1')
-    user = User.objects.get(id=task_id)
-    print(f"user {user}")
-    token, _ = ConfirmEmailToken.objects.get_or_create(user=user)
-    email = user.email
+def send_confirmation_email(user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        token, _ = ConfirmEmailToken.objects.get_or_create(user=user)
 
-    subject = 'Запрос на сброс пароля'
-    html_message = render_to_string('password_reset_email.html', {
-        'user': user,
-        'token': token.key,
-    })
-    plain_message = strip_tags(html_message)
+        subject = f"Подтверждение email для {user.email}"
+        html_message = render_to_string('confirmation_email.html', {
+            'user': user,
+            'token': token.key
+        })
+        plain_message = strip_tags(html_message)
 
-    send_mail(
-        subject,
-        plain_message,
-        settings.EMAIL_HOST_USER,
-        [email],
-        fail_silently=False,
-        html_message=html_message,
-    )
+        msg = EmailMultiAlternatives(
+            subject,
+            plain_message,
+            settings.EMAIL_HOST_USER,
+            [user.email]
+        )
+        msg.attach_alternative(html_message, "text/html")
+        msg.send()
 
-    print("Письмо с токеном сброса пароля отправлено на адрес {}".format(email))
+    except User.DoesNotExist:
+        pass
+
+
+@shared_task
+def send_order_email(user_id):
+    try:
+        user = User.objects.get(id=user_id)
+
+        subject = "Обновление статуса заказа"
+        html_message = render_to_string('order_email.html', {
+            'user': user
+        })
+        plain_message = strip_tags(html_message)
+
+        msg = EmailMultiAlternatives(
+            subject,
+            plain_message,
+            settings.EMAIL_HOST_USER,
+            [user.email]
+        )
+        msg.attach_alternative(html_message, "text/html")
+        msg.send()
+
+    except User.DoesNotExist:
+        pass
+
+
+@shared_task
+def send_password_reset_email(user_id, reset_password_token):
+    try:
+        user = User.objects.get(id=user_id)
+        token = ResetPasswordToken.objects.get(key=reset_password_token)
+
+        subject = f"Сброс пароля для {user.email}"
+        html_message = render_to_string('password_reset_email.html', {
+            'user': user,
+            'token': token.key
+        })
+
+        msg = EmailMultiAlternatives(
+            subject,
+            strip_tags(html_message),
+            settings.EMAIL_HOST_USER,
+            [user.email]
+        )
+        msg.attach_alternative(html_message, "text/html")
+        msg.send()
+
+    except (User.DoesNotExist, ResetPasswordToken.DoesNotExist):
+        pass
+
+
+@shared_task
+def reset_password_request_token(user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        token = ResetPasswordToken.objects.create(
+            user=user,
+            key=ResetPasswordToken.generate_key()
+        )
+        send_password_reset_email.delay(user.id, token.key)
+    except User.DoesNotExist:
+        pass
 
 
 @shared_task(bind=True)
